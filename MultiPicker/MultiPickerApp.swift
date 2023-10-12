fc7a2e1510b8b50292f62ad75f2850914a4ad4c4
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-10-12 15:11:44 +0300

    Initial Commit

---
+++ MultiPicker/MultiPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiPickerApp.swift
+//  MultiPicker
+//
+//  Created by Yunus Emre Taşdemir on 12.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct MultiPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
